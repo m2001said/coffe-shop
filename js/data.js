fba4671bb147cb53de7837ac858523a0c203707c
Mohamed Said <ms277712@gmail.com>
2023-01-16 07:08:14 +0200

    first commit

---
+++ js/data.js
@@ -0,0 +1,30 @@
+let coffeeData = [
+  {
+    id: "1",
+    name: "COFFEE CAPUCCINO",
+    price: "$5.90",
+    desc: "A small river named Duden flows by their place and supplies",
+    img: "/images/menu-1.jpg",
+  },
+  {
+    id: "2",
+    name: "COFFEE CAPUCCINO",
+    price: "$5.90",
+    desc: "A small river named Duden flows by their place and supplies",
+    img: "/images/menu-2.jpg",
+  },
+  {
+    id: "3",
+    name: "COFFEE CAPUCCINO",
+    price: "$5.90",
+    desc: "A small river named Duden flows by their place and supplies",
+    img: "/images/menu-3.jpg",
+  },
+  {
+    id: "3",
+    name: "COFFEE CAPUCCINO",
+    price: "$5.90",
+    desc: "A small river named Duden flows by their place and supplies",
+    img: "/images/menu-4.jpg",
+  },
+];
