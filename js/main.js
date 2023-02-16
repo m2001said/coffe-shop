20fae8827c68821d8b824c6ec7b4c9e61b2184a0
Mohamed Said <ms277712@gmail.com>
2023-02-16 09:38:40 +0200

    adding form validatino js

--- js/main.js
+++ js/main.js
@@ -107,3 +107,106 @@ setInterval(nextSlide, 6000);
 // Add event listeners to the buttons
 prevBtn.addEventListener("click", prevSlide);
 nextBtn.addEventListener("click", nextSlide);
+
+// -----form validation -----------
+const form = document.querySelector(".form");
+const formName = document.getElementById("name");
+const formEmail = document.getElementById("email");
+const formDate = document.getElementById("date");
+const formTime = document.getElementById("time");
+const formMessage = document.getElementById("message");
+const formPhone = document.getElementById("phone");
+const formSuccessMessage = document.querySelector(".form-succes");
+
+//for making error messages
+const setErrorFor = (input, message) => {
+  //get the parent dev of input to add classes for it
+  const inputContainer = input.parentElement;
+  inputContainer.classList.add("error-input");
+
+  const small = inputContainer.querySelector("small");
+  small.innerText = message;
+
+  const errorIcon = inputContainer.querySelector(".error-icon");
+  const successIcon = inputContainer.querySelector(".success-icon");
+  errorIcon.style.display = "block";
+  successIcon.style.display = "none";
+};
+
+//for making successful messages
+const setSuccessFor = (input) => {
+  const inputContainer = input.parentElement;
+  inputContainer.classList.add("success-input");
+
+  const small = inputContainer.querySelector("small");
+  small.innerText = "";
+
+  const errorIcon = inputContainer.querySelector(".error-icon");
+  const successIcon = inputContainer.querySelector(".success-icon");
+  successIcon.style.display = "block";
+  errorIcon.style.display = "none";
+};
+
+const checkInput = () => {
+  const nameValue = formName.value.trim();
+  const emailValue = formEmail.value.trim();
+  const dateValue = formDate.value.trim();
+  const timeValue = formTime.value.trim();
+  const messageValue = formMessage.value.trim();
+  const phoneValue = formPhone.value.trim();
+
+  if (nameValue === "") {
+    setErrorFor(formName, "Name input can't be empty");
+  } else {
+    setSuccessFor(formName);
+  }
+
+  if (emailValue === "") {
+    setErrorFor(formEmail, "Email input can't be empty");
+  } else {
+    setSuccessFor(formEmail);
+  }
+
+  if (dateValue === "") {
+    setErrorFor(formDate, "Date input can't be empty");
+  } else {
+    setSuccessFor(formDate);
+  }
+
+  if (timeValue === "") {
+    setErrorFor(formTime, "Time input can't be empty");
+  } else {
+    setSuccessFor(formTime);
+  }
+
+  if (messageValue === "") {
+    setErrorFor(formMessage, "Message input can't be empty");
+  } else {
+    setSuccessFor(formMessage);
+  }
+
+  if (phoneValue === "") {
+    setErrorFor(formPhone, "Phone input can't be empty");
+  } else {
+    setSuccessFor(formPhone);
+  }
+
+  // check if all inputs are valid
+  return (
+    nameValue !== "" &&
+    emailValue !== "" &&
+    dateValue !== "" &&
+    timeValue !== "" &&
+    messageValue !== "" &&
+    phoneValue !== ""
+  );
+};
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  checkInput()
+    ? ((formSuccessMessage.textContent = "You successfully book a table"),
+      (formSuccessMessage.style.color = "white"))
+    : ((formSuccessMessage.textContent = "You should complete all the input"),
+      (formSuccessMessage.style.color = "red"));
+});
